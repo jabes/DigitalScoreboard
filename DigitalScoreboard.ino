21b96b5fb26945a624786d3eff49ef476edbfa9b
Justin Bull <bull.justin@gmail.com>
2017-04-30 14:32:17 -0700

    Add second led control with player scores

--- DigitalScoreboard.ino
+++ DigitalScoreboard.ino
@@ -1,12 +1,11 @@
 #include <LedControl.h>
+LedControl lc1=LedControl(10,9,8,1);
+LedControl lc2=LedControl(13,12,11,1);
 
-/*
- Now we need a LedControl to work with.
- pin 12 is connected to the DataIn 
- pin 11 is connected to the CLK 
- pin 10 is connected to LOAD 
- */
-LedControl lc=LedControl(12,11,10,1);
+int score1 = 0;
+int score2 = 0;
+int lastScore1 = score1;
+int lastScore2 = score2;
 
 int Numbers[11][8][8] = {
   {
@@ -116,14 +115,21 @@ void setup() {
    The MAX72XX is in power-saving mode on startup,
    we have to do a wakeup call
    */
-  lc.shutdown(0,false);
+  lc1.shutdown(0,false);
+  lc2.shutdown(0,false);
   /* Set the brightness to a medium values */
-  lc.setIntensity(0,8);
+  lc1.setIntensity(0,8);
+  lc2.setIntensity(0,8);
   /* and clear the display */
-  lc.clearDisplay(0);
+  lc1.clearDisplay(0);
+  lc2.clearDisplay(0);
+  /* Set score */
+  setNumber(lc1, score1);
+  setNumber(lc2, score2);
 }
 
-void setNumber(int num) {
+void setNumber(LedControl lc, int num) {
+  lc.clearDisplay(0);
   for(int row=0;row<8;row++) {
     for(int col=0;col<8;col++) {
       boolean state = Numbers[num][row][col];
@@ -132,10 +138,17 @@ void setNumber(int num) {
   }
 }
 
-void loop() { 
-  lc.clearDisplay(0);
-  for(int num=0;num<11;num++) {
-    setNumber(num);
-    delay(500);
+void loop() {
+  if (lastScore1 != score1) {
+    setNumber(lc1, score1);
   }
+
+  if (lastScore2 != score2) {
+    setNumber(lc2, score2);
+  }
+
+  lastScore1 = score1;
+  lastScore2 = score2;
+
+  delay(500);
 }
